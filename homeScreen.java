1db33d03d3dad7de596f1cd24829e86d38eb66cd
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-25 11:43:27 -0700

    homescreen

--- homeScreen.java
+++ homeScreen.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+
 public class HomeScreen {
 
-public static void main(String[] args) {
-    System.out.println("Welcome to the online Store.\nHow can we assist you today?);
-}
+
+
+
+
+    public static void displayProducts(ArrayList<Product> products){
+       for(Product product : products){
+           System.out.println(product.toString());
+       }
+    }
 }
