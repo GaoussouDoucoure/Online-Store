4ffebef8cb96c62e5335974d04c6041084b02ad4
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-25 11:08:25 -0700

    homescreen

--- homeScreen.java
+++ homeScreen.java
@@ -1,5 +1,6 @@
-public class homeScreen {
-}
-public static void main(String[] args) {
+public class HomeScreen {
 
+public static void main(String[] args) {
+    System.out.println("Welcome to the online Store.\nHow can we assist you today?);
+}
 }
