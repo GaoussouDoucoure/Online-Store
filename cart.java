41c46951945727687639f8f87b4f81e3cbceff51
Gaoussou Doucoure <gaousd4@gmail.com>
2025-04-25 15:04:45 -0700

    homescreen

--- cart.java
+++
@@ -1,9 +0,0 @@
-public class cart {
-    public void cart(){
-        System.out.println("""
-                1 - Check Out
-                2 - Remove Product from the cart
-                0 - Go back to the home screen
-                """);
-    }
-}
--- store.java
+++ store.java
@@ -1,28 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class store {
-
-    public static void displayProducts(ArrayList<Product> products){
-        for(Product product : products){
+    // Store Home Screen
+    public static void storeHomeScreen(){
+        System.out.println("Welcome to our store!");
+    }
+    //Options for user to choose from
+    public static void option(){
+        System.out.println("""
+                Please enter a number to proceed:
+                1 - Display Products
+                2 - Display Cart
+                0 - Exit - closes out of the application
+                """);
+    }
+    //Products Screen
+    public static void products(){
+        System.out.println("""
+                1 - Search or Filter the list of Products
+                2 - Add a product to Cart
+                0 - Go back to Home Screen
+                """);
+    }
+    //Cart Screen
+    public static void cart(){
+        System.out.println("""
+                1 - Check out
+                2 - Remove Product from the Cart
+                0 - Go back to the Home Screen
+                """);
+    }
+    //Display all products in the inventory
+    public static void displayInventory(ArrayList<Product> inventory){
+        for(Product product : inventory){
             System.out.println(product.toString());
         }
     }
 
-    public void cart(){
-
-
-        System.out.println("""
-                1 - Check Out
-                2 - Remove Product from the cart
-                0 - Go back to the home screen
-                """);
-    }
     public static void main(String[] args) {
+        ArrayList<Product> inventory = new ArrayList<>();
+        try{
+            //Create a File object for the file to be read
+            File file = new File("products.txt");
+            //Create a scanner object to read the file
+            Scanner scanner = new Scanner(file);
 
-        Product p = new Product("a11", "nail", 8.50, "hardware");
-        System.out.println(p);
-        System.out.println("Welcome to the store\nHow can we assist you today?");
-        System.out.println("1.  ");
+            //Read and process each line from the file
+            while (scanner.hasNextLine()){
+                String line = scanner.next();
+                System.out.println(line);
 
+            }
+
+
+
+        }catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
     }
 }
